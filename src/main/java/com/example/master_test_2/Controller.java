6c2ac0c8820237f83b458f3d06a4133027375e9d
Georgepp24 <148490292+Georgepp24@users.noreply.github.com>
2024-12-20 18:34:06 +0000

    Init commit

---
+++ src/main/java/com/example/master_test_2/Controller.java
@@ -0,0 +1,13 @@
+package com.example.master_test_2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping(value="/hello")
+    public String hello() {
+        return "Hello World";
+    }
+}
---
+++ src/test/java/com/example/master_test_2/MasterTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.master_test_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterTest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
